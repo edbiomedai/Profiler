ee5182ac5323747f7e3aaa6346f2f06c262e2f67
Hugh Warden <h.b.warden@sms.ed.ac.uk>
2025-03-14 12:37:01 +0000

    Update QC erosion distance

--- profiler/analyse/_create_qc_mask.py
+++ profiler/analyse/_create_qc_mask.py
@@ -15,7 +15,7 @@ def create_qc_mask(args: Namespace, logger: Logger, paths: dict, benchmarks: dic
     tis_mask_meta = os.path.join(tis_mask_dir, "meta.json")
     tissue_mask_scematk = read_zarr_bin_mask(tis_mask_zarr, tis_mask_meta, mask_name="Tissue")
     tissue_mask = tissue_mask_scematk.image
-    footprint = disk(tissue_mask_scematk.pixel_from_micron(50))
+    footprint = disk(tissue_mask_scematk.pixel_from_micron(100))
     qc_mask = binary_erosion(tissue_mask, structure=footprint)
     qc_mask_labeled, _ = label(qc_mask)
     start_time = time()
