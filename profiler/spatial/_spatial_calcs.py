d8b2b2de979bb74e6c26bed05a2a09e17fbe132f
Hugh Warden <hugh.warden@outlook.com>
2025-04-13 19:07:46 +0100

    update

--- profiler/spatial/_spatial_calcs.py
+++ profiler/spatial/_spatial_calcs.py
@@ -2,25 +2,20 @@ import dask.array as da
 import dask.dataframe as dd
 import pandas as pd
 
-def save_dist_mat(df: dd.DataFrame, path: str) -> None:
+def save_dist_mat(df: dd.DataFrame, path: str, distance_threshold: int = 100) -> None:
     data = df[["Meta_Global_Mask_Label", "Meta_Nuclei_Mask_CentroidX", "Meta_Nuclei_Mask_CentroidY"]]
     coords = data[["Meta_Nuclei_Mask_CentroidX", "Meta_Nuclei_Mask_CentroidY"]].to_dask_array(lengths = True)
     coords = coords.rechunk((2048, 2))
     x_diff = coords[:, None, 0] - coords[None, :, 0]
     y_diff = coords[:, None, 1] - coords[None, :, 1]
-    da.sqrt(x_diff ** 2 + y_diff ** 2).to_zarr(path)
+    dist_mat = da.sqrt(x_diff ** 2 + y_diff ** 2)
+    threshold_dist_mat = da.less_equal(dist_mat, distance_threshold)
+    threshold_dist_mat[da.eye(threshold_dist_mat.shape[0], dtype=bool)] = False
+    threshold_dist_mat.to_zarr(path)
 
-def calculate_local_counts(df: dd.DataFrame, distance_threshold: int = 25) -> dd.DataFrame:
+def calculate_local_counts(dist_mat: da.Array, distance_threshold: int = 25) -> dd.DataFrame:
     out_var = f"Spatial_Nuclei_Mask_LocalCount{distance_threshold}"
-    data = df[["Meta_Global_Mask_Label", "Meta_Nuclei_Mask_CentroidX", "Meta_Nuclei_Mask_CentroidY"]]
-    coords = data[["Meta_Nuclei_Mask_CentroidX", "Meta_Nuclei_Mask_CentroidY"]].to_dask_array(lengths = True)
-    coords = coords.rechunk((2048, 2))
-    x_diff = coords[:, None, 0] - coords[None, :, 0]
-    y_diff = coords[:, None, 1] - coords[None, :, 1]
-    dist_mat = da.less_equal(da.sqrt(x_diff ** 2 + y_diff ** 2), distance_threshold).astype(int)
-    dist_mat[da.eye(dist_mat.shape[0], dtype=bool)] = 0
-    counts = dd.from_dask_array(dist_mat.sum(axis=1), columns=out_var)
-    return counts
+    return dd.from_dask_array(dist_mat.sum(axis=1), columns=out_var)
 
 def calculate_local_means(df: dd.DataFrame, var_name: str, distance_threshold: int = 25) -> dd.DataFrame:
     out_var = f"Spatial_Nuclei_Mask_LocalAvg{var_name.replace("_", "")}{distance_threshold}"
